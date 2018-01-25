6b5e70adb2834d088c9894a52e9702cb7e717df7
shabikaplan <35255517+shabikaplan@users.noreply.github.com>
2018-01-25 09:52:17 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'; 
+import logo from './logo.svg';
+import './App.css';
+
+let defaultStyle = {
+	color: '#fff'
+};
+
+class Aggregate extends Component {
+	render(){
+		return (
+		<div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+			<h2>Number Text</h2>
+		</div>
+		);
+	}
+}
+
+class Filter extends Component{
+	render(){
+		return(
+			<div style={defaultStyle}>
+				<img/>
+				<input type="text"/>
+			</div>
+		);
+	}
+}
+
+class Playlist extends Component{
+	render(){
+		return(
+			<div style={{...defaultStyle, display:"inline-block", width: "25%"}}>
+				<img/>
+				<h3>Playlist Name</h3>
+				<ul>
+					<li>Song 1</li>
+					<li>Song 2</li>
+					<li>Song 3</li>
+					<li>Song 4</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+  render() {
+	  let name = 'shabti'
+	  let headerStyle = 'lightblue'
+    return (
+		
+      <div className="App">
+        <h1 style={{color:'#fff'}}>Title</h1>
+		<Aggregate/>
+		<Aggregate/>
+		<Filter/>
+		<Playlist/>
+		<Playlist/>
+		<Playlist/>
+		<Playlist/>
+      </div>
+	  
+    );
+  }
+}
+
+
+export default App;
